987effef3261333051c4c1dbc0fca8ce4fc0f912
WebNaresh <nareshbhosale73@gmail.com>
2022-10-27 11:23:15 +0530

    27/10/2022

--- controller/testController.js
+++ controller/testController.js
@@ -1,44 +1,56 @@
 // testController a User
-const ErrorHandler = require('../utils/errorHandler');
-const catchAssyncError = require('../middleware/catchAssyncError');
-const User = require('../models/userSchema');
+const ErrorHandler = require("../utils/errorHandler");
+const catchAssyncError = require("../middleware/catchAssyncError");
+const User = require("../models/userSchema");
 const sendToken = require("../utils/sendToken");
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 exports.test = catchAssyncError(async (req, res, next) => {
-    res.status(200).json({ message: 'Route is  working ' });
-    // const {  } = req.body
+  res.status(200).json({ message: "Route is  working " });
+  // const {  } = req.body
 });
 
 exports.register = catchAssyncError(async (req, res, next) => {
-    const { name, email, password, userImage } = req.body;
-    const existed = await User
-        .find({ name, email })
-        .populate("name");
+  const { name, email, userImage } = req.body;
+  const existed = await User.find({ name, email }).populate("name");
 
-    if (existed.length === 1) {
-        existed[0].password = null;
-        return sendToken(existed[0], res, 201);
-    } else {
-
-        const user = await User.create({
-            name, email, userImage, password
-        });
-        user.password = null;
-        sendToken(user, res, 200);
-    }
-
+  if (existed.length === 1) {
+    return sendToken(existed[0], res, 201);
+  } else {
+    const user = await User.create({
+      name,
+      email,
+      userImage,
+    });
+    sendToken(user, res, 200);
+  }
 });
 // getAllUser a User
 
 exports.getAllUser = catchAssyncError(async (req, res, next) => {
-    const { userId } = req.body;
-    let users = await User.find({ _id: { $ne: userId } }).select(["name", "email", "userImage", "_id", "lastMessage", "lastMessageTime", "msgCondition"]);
+  const { userId } = req.body;
+  if (userId === undefined) {
+    console.log("i am undefinde");
+    return next(new ErrorHandler("provide Id", 400));
+  }
+  let users = await User.find({ _id: { $ne: userId } }).select([
+    "name",
+    "email",
+    "userImage",
+    "_id",
+  ]);
+  console.log(userId);
+  users.filter((data) => {
+    console.log(data._id);
+    return data._id.toString() !== userId;
+  });
+  console.log(users, "hello");
 
-    users = await jwt.sign({ users }, process.env.jWT_SECRETE, { expiresIn: process.env.JWT_EXPIRE });
-    res.status(200).json({
-        success: true,
-        users,
-    });
+  users = jwt.sign({ users }, process.env.jWT_SECRETE, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+  res.status(200).json({
+    success: true,
+    users,
+  });
 });
